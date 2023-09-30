0e0d7a234a8c3deeec7a4ff37726b2d69e069de2
liuxu89 <16112973+liuxu89@users.noreply.github.com>
2023-09-30 10:46:49 +0800

    Update indent-blankline.lua

--- neovim/nvim/lua/config/indent-blankline.lua
+++ neovim/nvim/lua/config/indent-blankline.lua
@@ -1,8 +1,13 @@
-vim.opt.list = true
+-- vim.opt.list = true
 vim.opt.listchars:append "space:⋅"
 -- vim.opt.listchars:append "eol:↴"
 
-require("indent_blankline").setup {
+require("ibl").setup {
+    indent = {
+        char = '│',
+        -- char = '⎸',
+        highlight = highlights,
+    },
     show_end_of_line = false,
     space_char_blankline = " ",
     show_current_context = true,
